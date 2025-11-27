59045af0142c25b6ee2065da9b0e8021f09d1036
Santiago Varas <santvaras92@gmail.com>
2025-11-27 08:39:04 -0300

    Remove conflicting wrapper in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
 import FlightUploadForm from "./components/FlightUploadForm";
 
 export default function Home() {
-  return (
-    <main className="min-h-screen bg-gray-50 py-8">
-      <FlightUploadForm />
-    </main>
-  );
+  return <FlightUploadForm />;
 }
